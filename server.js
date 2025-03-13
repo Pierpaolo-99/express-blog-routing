3730344c09cc865d400f04c36ef0d5db094a8c90
Pierpaolo-99 <pierpaolodellioio@gmail.com>
2025-03-13 13:18:19 +0100

    import routes and add middleware

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3004;
 
+// Import routes
+const postsRoute = require('./routers/postsRoute')
+
+// Listen to port
 app.listen(port, () => {
     console.log(`Server running on port:${port}`);
 })
@@ -11,3 +15,5 @@ app.get('/', (req,res) => {
     res.send('Welcome to home Blog!')
 })
 
+// middleware
+app.use('/api/v1/posts', postsRoute);
